039b1a5ccde38c9145b552a1afaffdf2d1c4d449
Will Stenzel <wstenzel@stu.naperville203.org>
2023-09-22 13:20:54 -0500

    it doesnt work

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -13,6 +13,32 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        Set<String> dictionaryWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> novelWords = readWords("Chapter 15 Class Notes/src/war-and-peace.txt");
+
+        //print the words in the novel
+        for(String word: novelWords)
+        {
+            if(!dictionaryWords.contains(word))
+            {
+                System.out.println(word);
+            }
+        }
+
+        //print number of words in novel
+        System.out.println("There are "+novelWords.size()+" unique words in the novel");
+
+        //print the number of unique >3 letter words
+        Iterator<String> iterator = novelWords.iterator();
+        while(iterator.hasNext())
+        {
+            if(iterator.next().length()>3)
+            {
+                iterator.remove();
+            }
+        }
+
+        System.out.print("There are "+novelWords.size()+" unique words with less than three letters");
     }
 
     /**
@@ -25,6 +51,21 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        Set<String> words = new HashSet<>();//no need to sort
+
+        //determine the current working directory
+        //System.out.println(System.getProperty("user.dir"));
+
+        Scanner scan  = new Scanner(new File(filename), "UTF-8");
+
+        //use any charecter thats not a letter as a delimeters
+        scan.useDelimiter("[^a-zA-Z]+");
+        while(scan.hasNext());
+        {
+            //add words to set, duplicates ignored
+            words.add(scan.next().toLowerCase());
+        }
+
+        return words;
     }
 }
