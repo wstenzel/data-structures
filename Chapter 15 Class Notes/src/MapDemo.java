9f01145e6243608f726e92658826bec2907fbd6a
Will Stenzel <wstenzel@stu.naperville203.org>
2023-09-28 14:05:17 -0500

    nm FZGmsthgm

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,20 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        Map<String, Color> favColors = new HashMap<>();
+        favColors.put("Peter", Color.BLACK);
+        favColors.put("Seth", Color.BLUE);
+        favColors.put("Celeste", Color.ORANGE);
+        favColors.put("Dr. Miller", Color.ORANGE);//two different keys, same value is fine
+        //same key cant have two different values
+        favColors.put("Peter", Color.YELLOW);//replaces black with yellow
+
+        //create a set of the keys in a map
+        Set<String> keys = favColors.keySet();
+        for(String key : keys)
+        {
+            System.out.println(key+" ("+key.hashCode()+"):" + favColors.get(key));
+        }
+
     }
 }
