1084af3adfaf8a5d0219cbbd60799ecf464c3802
Will Stenzel <wstenzel@stu.naperville203.org>
2023-10-20 14:13:05 -0500

    skink

--- Chapter 16 Class Notes/src/CircularArrayQueue.java
+++ Chapter 16 Class Notes/src/CircularArrayQueue.java
@@ -6,6 +6,9 @@ import java.util.NoSuchElementException;
 public class CircularArrayQueue
 {
     private Object[] elements;
+    private int head;
+    private int tail;
+    private int currentSize;
     //private data
 
 
@@ -13,7 +16,14 @@ public class CircularArrayQueue
     /**
         Constructs an empty queue.
     */
-
+    public CircularArrayQueue()
+    {
+        final int INITIAL_SIZE = 5;
+        this.elements = new Object[INITIAL_SIZE];
+        head = 0;
+        currentSize = 0;
+        tail = 0;
+    }
 
 
 
@@ -23,7 +33,10 @@ public class CircularArrayQueue
         Checks whether this queue is empty.
         @return true if this queue is empty
     */
-
+    public boolean empty()
+    {
+        return this.currentSize==0;
+    }
 
 
 
@@ -31,7 +44,13 @@ public class CircularArrayQueue
         Adds an element to the tail of this queue.
         @param newElement the element to add
     */
-
+    public void add(Object element)
+    {
+        this.currentSize++;
+        this.elements[this.tail]=element;
+        this.tail++;
+        this.tail%=this.elements.length;
+    }
 
 
 
@@ -40,7 +59,18 @@ public class CircularArrayQueue
         Removes an element from the head of this queue.
         @return the removed element
     */
+    public Object remove()
+    {
+        if(this.empty())
+        {
+            throw new NoSuchElementException();
+        }
 
+        this.currentSize--;
+        Object element = this.elements[this.head];
+        this.head=(this.head+1) % this.elements.length;
+        return element;
+    }
 
 
 
--- Chapter 16 Class Notes/src/LinkedListStack.java
+++ Chapter 16 Class Notes/src/LinkedListStack.java
@@ -20,7 +20,13 @@ public class LinkedListStack
      *
      * @param element the element to add
     */
-
+    public void push(Object element)
+    {
+        Node newNode = new Node();
+        newNode.data=element;
+        newNode.next=this.first;
+        this.first=newNode;
+    }
 
 
 
@@ -30,7 +36,16 @@ public class LinkedListStack
         Removes the element from the top of the stack.
         @return the removed element
     */
-
+    public Object pop()
+    {
+        if(this.empty())
+        {
+            throw new NoSuchElementException();
+        }
+        Object element = this.first.data;
+        this.first=this.first.next;
+        return element;
+    }
 
 
 
@@ -43,7 +58,10 @@ public class LinkedListStack
      *
      * @return true if the stack is empty
     */
-
+    public boolean empty()
+    {
+        return this.first==null;
+    }
 
     static class Node
     {
